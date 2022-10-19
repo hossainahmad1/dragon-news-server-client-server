e0c0e07e18d2bed1da9e330760468783cdbb539b
Hossain Ahmad <hossain177922@gmail.com>
2022-10-19 21:20:09 +0600

    basic backend server and dragon news

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/category.json');
+app.get('/', (req, res) => {
+    res.send('News and API running')
+})
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+app.listen(port, () => {
+    console.log('Dragon news server running on port', port);
+})
